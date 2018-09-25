b503150f24aa45a6ac0556a22fd0db560bfec4f2
binhui <binhui.ni@yourdream.cc>
2018-09-25 23:21:14 +0800

    save files

--- models/user.py
+++ models/user.py
@@ -6,15 +6,18 @@ class User(Model):
     User 是一个保存用户数据的 model
     现在只有两个属性 username 和 password
     """
+
     def __init__(self, form):
         self.id = form.get('id', None)
         self.username = form.get('username', '')
         self.password = form.get('password', '')
+        self.role_id = int(form.get('role_id', 11))
 
     def salted_password(self, password, salt='$!@><?>HUI&DWQa`'):
         import hashlib
         def sha256(ascii_str):
             return hashlib.sha256(ascii_str.encode('ascii')).hexdigest()
+
         hash1 = sha256(password)
         hash2 = sha256(hash1 + salt)
         return hash2
